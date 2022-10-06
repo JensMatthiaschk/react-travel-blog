61396c9a6c027ea60af59f207289b1b846dabb2f
JensMatthiaschk <jens.matthiaschk@gmail.com>
2022-10-06 19:53:29 +0200

    establish API connection and first JSX render

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,48 @@ import './App.css'
 
 function App() {
 
-  const [blogData, setBlogData] = useState(null);
+  const [blogData, setBlogData] = useState();
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setLoading(true)
+    // setLoading(true)
     fetch(`https://cdn.contentful.com/spaces/qiq9w8fj2md5/environments/master/entries?access_token=WNQszWbpf5NAWJLTmFTPYa3CH7uTBUr5NPLo1cYFttA`)
       .then(response => {
         if (response.ok) {
-          console.log(response)
+          // console.log(response)
           return response.json();
 
         }
         throw new Error('Request failed!');
       }, networkError => console.log(networkError.message)
-      ).then(data => {
+      )
+      .then(data => {
         setBlogData(data);
-        setLoading(false);
-        console.log(data);
+        // setLoading(false);
+        // console.log(data.items);
       })
       .catch((error) => {
         setError(error);
-        setLoading(false);
+        // setLoading(false);
       });
   }, [])
 
-  console.log(blogData.items)
+  console.log(blogData && blogData.items[0].fields.test1)
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      {/* <img src={`${blogData.items[0].fields.destinationImage[0].sys.id}`} alt="" /> */}
+      <h1>{blogData && blogData.items[0].fields.title}</h1>
+      <p>{blogData && blogData.items[0].fields.body}</p>
+      <img src={blogData && blogData.includes.Asset[0].fields.file.url + '?w=250'} alt="" />
+      <ul>
+        {blogData && blogData.items[0].fields.test1.map((item) =>
+          <li>{item}</li>
+        )}
+      </ul>
     </div>
   )
 }
 
 export default App
+
